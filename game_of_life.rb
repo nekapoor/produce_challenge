1f6fd0391df41f7d249abeecb7490480ba580701
Neeraj Kapoor <neeraj.kapoor@learntobe.org>
2018-08-09 11:55:37 -0700

    refactor into CellAutomata class

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,115 +1,151 @@
 require 'pp'
 
 FIVE_BY_FIVE = [[nil, nil, 1, nil, nil], [nil, nil, 1, 1, nil], [nil, 2, 2, 1, nil], [nil, nil, nil, 1, nil], [nil, nil, nil, nil, nil]]
+TEN_BY_TEN = [[nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, 1, 1, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, 2, nil, nil, nil, nil, nil], [nil, nil, nil, 1, 2, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, 1, nil, nil, nil, nil, nil, nil, nil], [nil, 2, 1, nil, nil, nil, nil, nil, nil, nil], [nil, 2, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]]
 
-def cell_type(row, column, arr)
-	return false unless is_valid_location?(row, column, arr)
-	case arr[row][column]
-	when 1
-	  return 'newborn'
-	when 2
-	  return 'adult'
-	when 3
-	  return 'senior'
-	else
-	  return nil
+
+class CellularAutomata
+
+	def initialize(arr)
+		@original_cells = arr
+		@cells = arr
+		@rows = arr.length
+		@cols = arr[0].length
 	end
-end
 
-def neighbor_newborn_count(current_row, current_column, arr)
-	neighbor_type_count(current_row, current_column, arr)[:newborn]
-end
-
-def neighbor_adult_count(current_row, current_column, arr)
-	neighbor_type_count(current_row, current_column, arr)[:adult]
-end
-
-def neighbor_senior_count(current_row, current_column, arr)
-	neighbor_type_count(current_row, current_column, arr)[:senior]
-end
-
-def neighbor_type_count(current_row, current_column, arr)
-	neighbor_hash = {:newborn => 0, :adult => 0, :senior => 0}
-	neighbors = surrounding_neighbor_locations(current_row, current_column, arr)
-	neighbors.each do |r, c|
-		type = cell_type(r, c, arr)
-		neighbor_hash[type.to_sym] += 1 if type
-	end
-	neighbor_hash
-end
-
-def total_neighbor_count(current_row, current_column, arr)
-	neighbor_type_count(current_row, current_column, arr).values.inject(0, :+)
-end
-
-def surrounding_neighbor_locations(current_row, current_column, arr)
-	locations = []
-	starting_row = current_row - 1
-	starting_col = current_column - 1
-	starting_row.upto(starting_row+2) do |r|
-		starting_col.upto(starting_col+2) do |c|
-			locations << [r, c] if is_valid_location?(r, c, arr)
+	def cells_after_generation!(gen_num)
+		return @cells if gen_num <= 1
+		gen_num.times do 
+			process_next_generation!
 		end
 	end
-	locations.delete([current_row, current_column])
-	locations 
-end
 
-def new_cell_for(current_row, current_column, arr)
-	case cell_type(current_row, current_column, arr)
-	when nil
-	  generate_new_cell_for_empty(current_row, current_column, arr)
-	when 'newborn'
-	  generate_new_cell_for_newborn(current_row, current_column, arr)
-	when 'adult'
-	  generate_new_cell_for_adult(current_row, current_column, arr)
-	else 
-	  return nil
+	def process_next_generation!
+		gen_arr = Array.new(@rows) { Array.new(@cols)}
+		@cells.each_with_index do |x, r|
+		  x.each_with_index do |y, c|
+				gen_arr[r][c] = new_cell_for(r, c)
+		  end
+		end
+		@cells = gen_arr
 	end
-end
 
-def generate_new_cell_for_empty(current_row, current_column, arr)
-	if neighbor_adult_count(current_row, current_column, arr) == 2
-		return 1
-	else
-		return nil
+	def cells
+		@cells
 	end
-end
 
-def generate_new_cell_for_newborn(current_row, current_column, arr)
-	if total_neighbor_count(current_row, current_column, arr) >= 5 || total_neighbor_count(current_row, current_column, arr) <= 1
-		return nil
-	else
-		return 2
+	def pretty_print_cells
+		pp @cells
 	end
-end
 
-def generate_new_cell_for_adult(current_row, current_column, arr)
-	if total_neighbor_count(current_row, current_column, arr) >= 3 || total_neighbor_count(current_row, current_column, arr) == 0
-		return nil
-	else
-		return 3
+	def pretty_print_original_cells
+		pp @original_cells
 	end
-end
 
-def is_valid_location?(row, column, arr)
-	return false if row > 4 || column > 4
-	return false if row < 0 || column < 0
-	true
-end
+	private 
 
-def process_next_generation(arr)
-	next_generation = Array.new(5) { Array.new(5)}
-	arr.each_with_index do |x, r|
-	  x.each_with_index do |y, c|
-			next_generation[r][c] = new_cell_for(r, c, arr)
-	  end
+	def new_cell_for(current_row, current_column)
+		case cell_type(current_row, current_column)
+		when nil
+		  generate_new_cell_for_empty(current_row, current_column)
+		when 'newborn'
+		  generate_new_cell_for_newborn(current_row, current_column)
+		when 'adult'
+		  generate_new_cell_for_adult(current_row, current_column)
+		when 'senior' 
+		  return nil
+		end
 	end
-	next_generation
+
+	def generate_new_cell_for_empty(current_row, current_column)
+		if neighbor_adult_count(current_row, current_column) == 2
+			return 1
+		else
+			return nil
+		end
+	end
+
+	def generate_new_cell_for_newborn(current_row, current_column)
+		total = total_neighbor_count(current_row, current_column)
+		if total >= 5 || total <= 1
+			return nil
+		else
+			return 2
+		end
+	end
+
+	def generate_new_cell_for_adult(current_row, current_column)
+		total = total_neighbor_count(current_row, current_column)
+		if total >= 3 || total == 0
+			return nil
+		else
+			return 3
+		end
+	end
+
+	def surrounding_neighbor_locations(current_row, current_column)
+		locations = []
+		starting_row = current_row - 1
+		starting_col = current_column - 1
+		starting_row.upto(starting_row+2) do |r|
+			starting_col.upto(starting_col+2) do |c|
+				locations << [r, c] if is_valid_location?(r, c)
+			end
+		end
+		locations.delete([current_row, current_column])
+		locations 
+	end
+
+
+	def neighbor_newborn_count(current_row, current_column)
+		neighbor_type_count(current_row, current_column)[:newborn]
+	end
+
+	def neighbor_adult_count(current_row, current_column)
+		neighbor_type_count(current_row, current_column)[:adult]
+	end
+
+	def neighbor_senior_count(current_row, current_column)
+		neighbor_type_count(current_row, current_column)[:senior]
+	end
+
+	def neighbor_type_count(current_row, current_column)
+		neighbor_hash = {:newborn => 0, :adult => 0, :senior => 0}
+		neighbors = surrounding_neighbor_locations(current_row, current_column)
+		neighbors.each do |r, c|
+			type = cell_type(r, c)
+			neighbor_hash[type.to_sym] += 1 if type
+		end
+		neighbor_hash
+	end
+
+	def total_neighbor_count(current_row, current_column)
+		neighbor_type_count(current_row, current_column).values.inject(0, :+)
+	end
+
+	def cell_type(row, column)
+		return false unless is_valid_location?(row, column)
+		case @cells[row][column]
+		when 1
+		  return 'newborn'
+		when 2
+		  return 'adult'
+		when 3
+		  return 'senior'
+		else
+		  return nil
+		end
+	end
+
+	def is_valid_location?(row, column)
+		return false if row > @rows-1 || column > @cols-1
+		return false if row < 0 || column < 0
+		true
+	end
+
 end
 
-pp FIVE_BY_FIVE
-pp process_next_generation(FIVE_BY_FIVE)
-
-
-p new_cell_for(1, 1, FIVE_BY_FIVE)
+@ca = CellularAutomata.new(TEN_BY_TEN)
+@ca.cells_after_generation!(20)
+@ca.pretty_print_original_cells
+@ca.pretty_print_cells
