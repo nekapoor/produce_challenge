f30cb959d5608d352c2ecb855e3818770348f01e
Neeraj Kapoor <neeraj.kapoor@learntobe.org>
2018-08-09 13:46:32 -0700

    update README with working rpsec comment and write a few more tests

--- spec/cellular_automata_spec.rb
+++ spec/cellular_automata_spec.rb
@@ -4,8 +4,7 @@ require 'spec_helper'
 describe CellularAutomata do
 	before do
 		TEN_BY_TEN = [[nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, 1, 1, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, 2, nil, nil, nil, nil, nil], [nil, nil, nil, 1, 2, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, 1, nil, nil, nil, nil, nil, nil, nil], [nil, 2, 1, nil, nil, nil, nil, nil, nil, nil], [nil, 2, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]]
-		@ca = CellularAutomata.new(TEN_BY_TEN)
-
+		@ca = CellularAutomata.new(TEN_BY_TEN, generations = 1)
 	end
 
 	describe "#is_valid_location?" do
@@ -34,5 +33,13 @@ describe CellularAutomata do
 		it "should return 3 locations for (0, 0)" do
 			expect(@ca.surrounding_neighbor_locations(0, 0)).to eq [[0, 1], [1, 0], [1, 1]]
 		end
+		it "should return 8 locations for (4, 4)" do
+			output = [[3, 3], [3, 4], [3, 5], [4, 3], [4, 5], [5, 3], [5, 4], [5, 5]]
+			expect(@ca.surrounding_neighbor_locations(4, 4)).to eq output
+		end
+		it "should return 5 locations for (5, 9)" do
+			output = [[4, 8], [4, 9], [5, 8], [6, 8], [6, 9]]
+		end
 	end
+
 end
