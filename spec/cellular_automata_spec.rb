caaf4c0e0fb74b5e5c8c896a9999f4e770fa775e
Neeraj Kapoor <neeraj.kapoor@learntobe.org>
2018-08-09 13:40:10 -0700

    make rspec work and refactor cellular_automata

--- spec/cellular_automata_spec.rb
+++ spec/cellular_automata_spec.rb
@@ -1,4 +1,7 @@
-describe "CellularAutomata" do
+require './cellular_automata'
+require 'spec_helper'
+
+describe CellularAutomata do
 	before do
 		TEN_BY_TEN = [[nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, 1, 1, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, 2, nil, nil, nil, nil, nil], [nil, nil, nil, 1, 2, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, 1, nil, nil, nil, nil, nil, nil, nil], [nil, 2, 1, nil, nil, nil, nil, nil, nil, nil], [nil, 2, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]]
 		@ca = CellularAutomata.new(TEN_BY_TEN)
@@ -7,29 +10,29 @@ describe "CellularAutomata" do
 
 	describe "#is_valid_location?" do
 		it "should return false for bad row and column" do
-			expect @ca.is_valid_location?(-1, -1).to eq false
+			expect(@ca.is_valid_location?(-1, -1)).to eq false
 		end
 
 		it "should return false for bad row and column" do
-			expect @ca.is_valid_location?(100, 100).to eq false
+			expect(@ca.is_valid_location?(100, 100)).to eq false
 		end
 	end
 
 	describe "#cell_type" do
 		it "should return newborn for (1, 2)" do 
-			expect @ca.cell_type(1, 2).to eq "newborn"
+			expect(@ca.cell_type(1, 2)).to eq "newborn"
 		end
 		it "should return adult for (2, 4)" do 
-			expect @ca.cell_type(2, 4).to eq "adult"
+			expect(@ca.cell_type(2, 4)).to eq "adult"
 		end
 		it "should return nil for (0, 0)" do 
-			expect @ca.cell_type(0, 0).to eq nil
+			expect(@ca.cell_type(0, 0)).to eq nil
 		end
 	end
 
 	describe "#surrounding_neighbor_locations" do
 		it "should return 3 locations for (0, 0)" do
-			expect @ca.surrounding_neighbor_locations(0, 0).to eq [[0, 1], [1, 0], [1, ]]
+			expect(@ca.surrounding_neighbor_locations(0, 0)).to eq [[0, 1], [1, 0], [1, 1]]
 		end
 	end
 end
